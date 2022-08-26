9e40ade7bb7ba034b403faca0d5f3984842378a0
Vinicius Maia <viniciusmaiamarinho1@gmail.com>
2022-08-26 11:17:45 -0300

    add test array char

---
+++ test-array-char.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+
+  char nomes[][10] = {"nome","nome3","opuuhdsapio","piudas"}; // Determina uma array para o char e o segundo [] determina o tamanho de cada item da array
+
+  for(int i = 0; i < sizeof(nomes)/sizeof(nomes[0]);i++)
+  {
+    printf("%s\n",nomes[i]);
+  }
+
+  return 0;
+}
