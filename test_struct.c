11278788c6c109fe8929ac2cd2b86e780f1b355b
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-09-09 13:12:24 -0300

    add estudo de struct

---
+++ test_struct.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+struct pessoa {
+    int idade;
+    float peso;
+    char nome[25];
+};
+
+int main(){
+    struct pessoa s1;
+    int id;
+    float pe;
+    char nam[25];
+    printf("Input your age: ");
+    scanf("%d",&id);
+    printf("\nInput your weight: ");
+    scanf("%f",&pe);
+    printf("\nInput your name: ");
+    scanf("%s",nam);
+    s1.idade = id;
+    s1.peso = pe;
+    strcpy(s1.nome,nam);
+    printf("\nHello %s your age is %d and your weight is %.2f\n",s1.nome,s1.idade,s1.peso);
+    return 0;
+}
+
+
+
