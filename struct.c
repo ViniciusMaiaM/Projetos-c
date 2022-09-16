5bb6562d023c123fdb1929fbb6e596097d26d8b9
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-09-16 13:54:34 -0300

    add more study with struct

--- struct.c
+++ struct.c
@@ -10,23 +10,28 @@ typedef struct{
 //Utilizing the typedef with struct there will be no need to call struct everytime
 
 void print_struct(pessoa p);
+void set_pessoa(pessoa *p, char name[25], int age, float weight );
 
 int main(){
     pessoa test;
     char nome[25];int ida; float pes;
     printf("Input your name: ");
     scanf("%s",nome);
-    strcpy(test.name,nome);
     printf("\nInput your age: ");
     scanf("%d",&ida);
-    test.age = ida;
     printf("\nInput your weight: ");
     scanf("%f",&pes);
-    test.weight = pes;
+    set_pessoa(&test, nome, ida, pes);
     print_struct(test);
     return 0;
 }
 
 void print_struct(pessoa p){
     printf("\nName: %s \nAge: %d \nWeight: %.2f\n",p.name,p.age,p.weight);
+}
+
+void set_pessoa(pessoa *p, char name[25], int age, float weight){
+    (*p).age = age;
+    p -> weight = weight;
+    strcpy( p-> name,name);
 }
