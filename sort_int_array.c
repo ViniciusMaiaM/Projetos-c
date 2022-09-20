92d55ce75958f81f6d838207c9baf7060096a490
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-09-20 10:51:47 -0300

    add desc sort function

--- sort_int_array.c
+++ sort_int_array.c
@@ -2,12 +2,25 @@
 
 void sort(int list[], int tam);
 void print(int list[], int tam);
+void sortdesc(int list[], int tam);
 
 void sort(int list[], int tam) //Ordenação manual de uma array
 {
   for (int i = 0; i < tam - 1; i++){
     for (int j = 0; j < tam - 1; j++){
       if (list[j] > list[j + 1]){
+        int save = list[j];
+        list[j] = list[j + 1];
+        list[j + 1] = save;
+      }
+    }
+  }
+}
+
+void sortdesc(int list[],int tam){
+  for (int i = 0; i < tam - 1; i++){
+    for (int j = 0; j < tam - 1; j++){
+      if (list[j] < list[j + 1]){
         int save = list[j];
         list[j] = list[j + 1];
         list[j + 1] = save;
@@ -26,7 +39,7 @@ void showlist(int list[], int tam)
 int main(){
   int list[] = {4371, 90, 42, 1, 6, 9, 3, 2, 5, 8, 4, 7}; 
   int tamanho_list = sizeof(list) / sizeof(list[0]); //Declaração do sizeof pra rodar no for
-  sort(list, tamanho_list);
+  sortdesc(list, tamanho_list);
   showlist(list, tamanho_list);
   return 0;
 }
