094391ab9564fed3ff05261e28aa244f0a9bf091
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-09-27 11:15:44 -0300

    add intial structure of the code

--- insertion_sort.c
+++ insertion_sort.c
@@ -3,9 +3,22 @@
 void sort(int array[], int size);
 
 int main(){
+    int size;
+    int search;
+    int result;
 
+    printf("Input the size of the array: ");
+    scanf("%d",&size);
+    int array[size];
 
+    for(int i = 0; i < size; i++){
+        printf("Input the %d term of your array: ",i+1);
+        scanf("%d",&array[i]);
+    }
 
+    sort(array, size);
+
+    
 
     return 0;
 }
@@ -24,5 +37,5 @@ void sort(int array[], int size){
 
         array[j+1] = save;
     }
-    
+
 }
