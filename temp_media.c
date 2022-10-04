de1c6b5467938352e03ed71d05938e09efad88b5
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-10-04 13:14:41 -0300

    calcul avareg temp

---
+++ temp_media.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int size = 0;
+    float temp = 0, med = 0.0;
+    printf("Input the number of values of temp: ");
+    scanf("%d",&size);
+
+    printf("Now input the values:\n");
+    for(int i = 0; i < size; i++){
+        printf("Term %d: ",i++);
+        scanf("%f",&temp);
+        med += temp;
+        temp = 0;
+    }
+
+    med /= size;
+    printf("Average temperature: %.2f Cº\n",med);
+    return 0;
+}
+
+// Baseado no código do professor flavius gorgonio
