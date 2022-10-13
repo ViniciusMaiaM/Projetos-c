e0edb0956e593a44f9b0c63cd9cbdd7a81ae145f
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-10-13 06:19:35 -0300

    add validação inicial de email

---
+++ valida_email.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+int valida_email(char email[]);
+
+int main(){
+
+    char email[] = {"teste@.com.br"};
+
+    if(valida_email(email)){
+        printf("\nDeu certo!\n");
+        return 0;
+    }
+
+    printf("\nDeu errado!\n");
+    return 0;
+}
+
+int valida_email(char email[]){
+
+    int cont = 0, arroba = 0, pont = 0;
+    
+    if (strlen(email) > 0){
+        for(int i = 0; i < strlen(email); i++){
+            
+            if(email[i] == '@'){
+                arroba ++;
+                cont++;
+            }
+
+            else if(email[i] == '.'){
+                cont ++;
+                pont++;
+
+            }
+        }
+
+        if(arroba == 1 && pont == 2){
+            return 1;
+        }
+
+        else{
+            return 0;
+        }
+    }
+    return 0;
+}
