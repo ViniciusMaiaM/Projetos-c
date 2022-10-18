26c0f92d01dacd9082f77efcbf1720f1a237d81c
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-10-18 13:10:02 -0300

    add função de maisculo e minusculo

---
+++ mai_min.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+char mai_min(char letra);
+
+int main(){
+
+    char digi;
+
+    printf("Insira um caracter: ");
+    scanf("%c",&digi);
+
+    digi  = mai_min(digi);
+
+    printf("\n%c\n",digi);
+
+    return 0;
+}
+
+char mai_min(char letra){
+
+    if (letra >= 'a' && letra<='z'){
+        return letra -32;
+    }
+    return letra + 32;
+}
