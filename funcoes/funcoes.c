b8a1ee2969c8460d89188e964b95829431c8e45f
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-10-26 10:23:48 -0300

    add estudo de malloc e função de variancia!

--- funcoes/funcoes.c
+++ funcoes/funcoes.c
@@ -28,3 +28,13 @@ void fibo(int size){
         t2 += save;
     }
 }
+
+
+float* variancia(float* u, float* v){
+    float *p = (float*) malloc(3*sizeof(float));
+    p[0] = u[1]*v[2] - v[1]*u[2];
+    p[1] = u[2]*v[0] - v[2]*u[0];
+    p[2] = u[0]*v[1] - v[0]*u[1];
+    return p;
+}
+// Dessa maneira declaramos o espaço do vetor na memória do programa e conseguimos retorna-lo
--- funcoes/funcoes.h
+++ funcoes/funcoes.h
@@ -2,3 +2,4 @@ int fat(int num);
 int arran (int n, int k);
 int comb(int n, int k);
 void fibo(int size);
+float* variancia( float* u, float* v);
